0e008dd1ad2f0566c4b74491e6f2c42a14d6e364
Flchsh <kat13smarjaanah@kamk.fi>
2014-10-03 16:23:05 +0300

    projekti

--- UtH-develop/vs2013/TestProject/source/Caveman.cpp
+++ UtH-develop/vs2013/TestProject/source/Caveman.cpp
@@ -4,14 +4,14 @@ void Caveman::Init(PhysicsWorld *world)
 {
 	speed = 3;
 	this->AddComponent(new Sprite("caveman.png"));
-	this->transform.SetPosition(0, 0);
+	this->transform.SetPosition(200, 200);
 	this->AddComponent(new Rigidbody(*world, COLLIDER_BALL));
 	this->GetComponent<Rigidbody>()->SetVelocity(pmath::Vec2(0, 0));
 }
 
 void Caveman::Hit()
 {
-	//animation
+	//animation not done!!!!!
 
 
 }
@@ -20,6 +20,7 @@ void Caveman::ChangeDirection(pmath::Vec2 pullPosition)
 	pmath::Vec2 temp = this->transform.GetPosition() - pullPosition;
 	temp.normalize();
 	this->GetComponent<Rigidbody>()->SetVelocity(temp * speed);
+
 }
 
 
