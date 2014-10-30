3b6e2a4aa67c03c67df8879ecf3f68d6e498df52
Taistelubanaani <munkkinen92@gmail.com>
2014-10-30 14:09:23 +0200

    Update and Draw for Stats
    NOT TESTED!

--- UtH-develop/vs2013/TestProject/include/Statistics.h
+++ UtH-develop/vs2013/TestProject/include/Statistics.h
@@ -4,22 +4,31 @@
 #include <UtH/UtHEngine.hpp>
 #include <Caveman.h>
 
+using namespace std;
 using namespace uth;
 
-class Statistics : public GameObject
+class Statistics
 {
 public:
-	inline Statistics() { ResetScore(); }
-	inline ~Statistics() {}
+	Statistics();
+	~Statistics();
 
-	void update(float dt) override final;
+	void Update(float dt);
+	void Draw(RenderTarget& target);
 	void ResetScore();
 
 private:
+	void UpdateOxygenBar(float dt);
+	void UpdateScore();
+	void UpdateGameTime(float dt);
+
 	float maxOxygen;
 	float oxygenLeft;
 	float gameTime;
 	int score;
+	GameObject* p_oxygenBar;
+	GameObject* p_scoreText;
+	GameObject* p_gameTimeText;
 };
 
 #endif STATISTICS_H
--- UtH-develop/vs2013/TestProject/source/Statistics.cpp
+++ UtH-develop/vs2013/TestProject/source/Statistics.cpp
@@ -1,18 +1,74 @@
 #include <Statistics.h>
 
-void Statistics::update(float dt)
+Statistics::Statistics()
 {
-	if (oxygenLeft > maxOxygen)
-		oxygenLeft = maxOxygen;
+	ResetScore();
 
-	oxygenLeft -= maxOxygen * 0.01f / 60;
-	gameTime += dt;
+	p_oxygenBar = new GameObject();
+	p_oxygenBar->AddComponent(new Sprite("Placeholders/oxygenbar_placeholder"));
+	p_oxygenBar->transform.SetPosition(uthEngine.GetWindow().GetViewport().getLeft() + 20, uthEngine.GetWindow().GetViewport().getTop() + 20);
+
+	p_scoreText = new GameObject();
+	p_scoreText->AddComponent(new Text("FreePixel", 50, "Score"));
+	p_scoreText->GetComponent<Text>("Score")->SetText(to_string(score));
+	p_scoreText->transform.SetPosition(uthEngine.GetWindow().GetViewport().getRight() - 20, uthEngine.GetWindow().GetViewport().getTop() + 20);
+
+	p_gameTimeText = new GameObject();
+	p_gameTimeText->AddComponent(new Text("FreePixel", 50, "GameTime"));
+	p_gameTimeText->GetComponent<Text>("GameTime")->SetText(to_string(gameTime));
+	p_gameTimeText->transform.SetPosition(uthEngine.GetWindow().GetViewport().center().w, uthEngine.GetWindow().GetViewport().getTop() + 20);
 }
-
+Statistics::~Statistics()
+{
+	delete(p_oxygenBar);
+	delete(p_scoreText);
+	delete(p_gameTimeText);
+}
+void Statistics::Update(float dt)
+{
+	//transform.SetPosition(uthEngine.GetWindow().GetCamera().GetPosition());
+	UpdateOxygenBar(dt);
+	UpdateScore();
+	UpdateGameTime(dt);
+}
+void Statistics::Draw(RenderTarget& target)
+{
+	p_gameTimeText->Draw(target);
+	p_scoreText->Draw(target);
+	p_oxygenBar->Draw(target);
+}
 void Statistics::ResetScore()
 {
 	maxOxygen = 1;
 	oxygenLeft = maxOxygen;
 	gameTime = 0;
 	score = 0;
+}
+
+void Statistics::UpdateOxygenBar(float dt)
+{
+	// Percentual decrease of oxygen / frames per second
+	oxygenLeft -= maxOxygen * 0.01f / (1 / (dt * 0.001f));
+
+	if (oxygenLeft > maxOxygen)
+		oxygenLeft = maxOxygen;
+	else if (oxygenLeft < 0)
+		oxygenLeft = 0;
+
+	p_oxygenBar->transform.SetPosition(uthEngine.GetWindow().GetViewport().getLeft() + 20, uthEngine.GetWindow().GetViewport().getTop() + 20);
+	p_oxygenBar->transform.SetScale(oxygenLeft, 1);
+	p_oxygenBar->transform.SetPosition(
+										p_oxygenBar->transform.GetPosition().x -
+					  p_oxygenBar->GetComponent<Sprite>("Sprite")->GetSize().x *
+																	oxygenLeft ,
+									   p_oxygenBar->transform.GetPosition().y);
+}
+void Statistics::UpdateScore()
+{
+	p_scoreText->transform.SetPosition(uthEngine.GetWindow().GetViewport().getRight() - 20, uthEngine.GetWindow().GetViewport().getTop() + 20);
+}
+void Statistics::UpdateGameTime(float dt)
+{
+	gameTime += dt;
+	p_gameTimeText->transform.SetPosition(uthEngine.GetWindow().GetViewport().center().w, uthEngine.GetWindow().GetViewport().getTop() + 20);
 }
